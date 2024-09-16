443cc450cc35029c0fafdeb94f01ab305112f6ce
Brendon Simonsen <brendon.simonsen@gmail.com>
2024-09-16 08:17:26 -0600

    creating classes to represent world

--- wumpus_world-master/PyAgent.py
+++ wumpus_world-master/PyAgent.py
@@ -3,6 +3,83 @@
 import Action
 import Orientation
 
+# class created to represent each cell of the map
+class Cell:
+    def __init__(self):
+        self.pit = False
+        self.wumpus = False
+        self.gold = False
+        self.visited = False
+        self.breeze = False
+        self.stench = False
+
+# class to help organize the wumpus world
+class World:
+    def __init__(self, size):
+        self.size = size
+        self.grid = [[Cell() for _ in range(size)] for _ in range(size)]
+        self.agent_x = 0
+        self.agent_y = 0
+
+# function for interpreting percepts
+def update_knowledge_from_percepts(world, x, y, breeze, stench):
+    cell = world.grid[x][y]
+    cell.visited = True
+    cell.breeze = breeze
+    cell.stench = stench
+
+    if breeze:
+        if x > 0:
+            world.grid[x-1][y].pit = True  # Assuming the worst case; refine this later with more logic.
+        if x < world.size - 1:
+            world.grid[x+1][y].pit = True
+        if y > 0:
+            world.grid[x][y-1].pit = True
+        if y < world.size - 1:
+            world.grid[x][y+1].pit = True
+
+    if stench:
+        if x > 0:
+            world.grid[x-1][y].wumpus = True
+        if x < world.size - 1:
+            world.grid[x+1][y].wumpus = True
+        if y > 0:
+            world.grid[x][y-1].wumpus = True
+        if y < world.size - 1:
+            world.grid[x][y+1].wumpus = True
+
+# function for deciding safe locations
+def infer_safe_locations(world):
+    for x in range(world.size):
+        for y in range(world.size):
+            cell = world.grid[x][y]
+            # A cell is safe if it has been visited or if it has no adjacent pits or Wumpus
+            adjacent_safe = True
+            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < world.size and 0 <= ny < world.size:
+                    if world.grid[nx][ny].pit or world.grid[nx][ny].wumpus:
+                        adjacent_safe = False
+            if cell.visited or (not cell.pit and not cell.wumpus and adjacent_safe):
+                cell.safe = True
+
+# function to hopefully choose a good next move
+def choose_next_move(world):
+    # Example strategy: find the closest safe, unvisited cell
+    from collections import deque
+    queue = deque([(world.agent_x, world.agent_y)])
+    visited = set((world.agent_x, world.agent_y))
+    while queue:
+        x, y = queue.popleft()
+        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < world.size and 0 <= ny < world.size and (nx, ny) not in visited:
+                if world.grid[nx][ny].safe and not world.grid[nx][ny].visited:
+                    return nx, ny
+                visited.add((nx, ny))
+                queue.append((nx, ny))
+    return None  # No move found
+
 
 def PyAgent_Constructor():
     """ PyAgent_Constructor: called at the start of a new trial """
@@ -16,6 +93,9 @@ def PyAgent_Destructor():
 
 def PyAgent_Initialize():
     """ PyAgent_Initialize: called at the start of a new try """
+
+    myworld = World(4)
+
     print("PyAgent_Initialize")
 
 
